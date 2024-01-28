399fb5e44f0ab8be37e2256d1ae3649fa71b0b78
epolewski <19355088+epolewski@users.noreply.github.com>
2024-01-27 20:09:40 -0600

    Add files via upload

--- ericLLM.py
+++ ericLLM.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.responses import PlainTextResponse
 from starlette.concurrency import run_until_first_complete
+import requests
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -64,6 +65,13 @@ def parse_args():
                         help='Use 8 bit cache (not implemented)')
     parser.add_argument('--num_workers', metavar='NUM_WORKERS', type=int, default=1,
                         help='Number of worker processes to use')
+     # Add a new command-line option for cloud management
+    default_config_api_url = 'https://default-config-api.com/config'
+    parser.add_argument('--managed', metavar='MANAGED', type=str, default=default_config_api_url,
+                        help=f'Enables online management of this server. (default: {default_config_api_url})')
+    default_config_engine = "ExLlamaV2"
+    parser.add_argument('--engine', metavar='ENGINE', type=str, default=default_config_engine,
+                        help=f'Choose the underlying engine of this server. Supports vLLM and ExLlamaV2 in quotes (default: {default_config_engine})')
 
 
 
@@ -78,6 +86,10 @@ MAX_PROMPTS = args.max_prompts
 
 
 app = FastAPI()
+
+if(args.engine == "vLLM"):
+    from vllm import LLM, SamplingParams
+
 
 class PromptRequest(BaseModel):
     prompt: str
@@ -104,137 +116,177 @@ loras = []
 async def process_input(input_id):
     print()
 
-async def inference_loop():
-    global prompts_queue, results, token_count, processing_started, total_processing_time
-    processing_started = True
-    token_processing_start_time = None
+try:
+    async def inference_loop():
+        global prompts_queue, results, token_count, processing_started, total_processing_time
+        processing_started = True
+        token_processing_start_time = None
 
 
-    while processing_started:
-        # Quick sleep to let the API server send back requests, or it waits serially for all these to finish
-        await asyncio.sleep(0.01)
-        if prompts_queue.qsize() == 0:
-            await asyncio.sleep(0.1)
-            continue  # No prompts to process yet.
+        while processing_started:
+            # Quick sleep to let the API server send back requests, or it waits serially for all these to finish
+            await asyncio.sleep(0.01)
+            if prompts_queue.qsize() == 0:
+                await asyncio.sleep(0.1)
+                continue  # No prompts to process yet.
 
-        # Start timing the token processing
-        #if token_processing_start_time is None:
-        token_processing_start_time = time.time()
-        token_count['read_tokens'] = 0
-        token_count['prompt_tokens'] = 0
-        token_count['gen_tokens'] = 0
+            # Start timing the token processing
+            #if token_processing_start_time is None:
+            token_processing_start_time = time.time()
+            token_count['read_tokens'] = 0
+            token_count['prompt_tokens'] = 0
+            token_count['gen_tokens'] = 0
 
-        input_ids = []
-        caches = []
-        settings = []
+            input_ids = []
+            caches = []
+            settings = []
 
-        ids_lookup = {}
-        settings_proto = ExLlamaV2Sampler.Settings()
+            ids_lookup = {}
+            settings_proto = ExLlamaV2Sampler.Settings()
 
-        prompt_count = 0
-        print(f"Starting at {time.time()}")
-        for _ in range(min(MAX_PROMPTS, prompts_queue.qsize())):
+            prompt_count = 0
+            print(f"Starting at {time.time()}")
+            for _ in range(min(MAX_PROMPTS, prompts_queue.qsize())):
 
-            ids, response_event, max_tokens, temperature, top_k, top_p, token_repetition_penalty, stop = await prompts_queue.get()
-            prompt_count += ids.size(1)
-            batch_size = 1
-            #if prompts_queue.qsize() % 2 == 0:
-                #batch_size = 2
-            cache = ExLlamaV2Cache(model, max_seq_len=(ids.size(1) + max_tokens), batch_size = batch_size)
-            #print(ids)
-            model.forward(ids[:, :-1], cache, preprocess_only=True)
-            input_ids.append(ids)
-            caches.append(cache)
-            settings_clone = settings_proto.clone()
-            settings_clone.temperature = temperature
-            settings_clone.top_p = top_p
-            settings_clone.top_k = top_k
-            settings_clone.token_repetition_penalty = token_repetition_penalty
-            settings_clone.batch_size = 1
+                ids, response_event, max_tokens, temperature, top_k, top_p, token_repetition_penalty, stop, prompt = await prompts_queue.get()
+                #print(prompt)
+                if(args.engine == "vLLM"):
+                    sampling_params = SamplingParams(temperature=temperature, top_p=top_p,max_tokens=max_tokens, top_k=top_k)
+                    outputs = model.generate(prompt,sampling_params)
+                    #print(f"Outputs {outputs}")
+                    #print(f"Outputs[0] {outputs[0]}")
 
-            #settings_clone.vocab_size = 8192
-            #settings_clone.max_attention_size = 2048 ** 16
-            #settings_clone.max_input_len = 8192
-            #settings_clone.max_seq_len = 8192
-            #settings_clone.hidden_size = 8192
-            #settings_clone.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
-            settings_clone.eos_token_id = 2
-            settings_clone.eos_token = "</s>"
-
-            #settings.append(settings_proto.clone())
-            settings.append(settings_clone)
-            ids_lookup[len(input_ids) - 1] = response_event #Should I change this to a hash? No because duplicates would still happen?
-
-        token_count['read_tokens'] += prompt_count
-        print(f"Doing input_ids at {time.time()}")
-        while input_ids:
-            inputs = torch.cat([x[:, -1:] for x in input_ids], dim=0)
-            logits = model.forward(inputs, caches, input_mask=None, loras = loras).float().cpu()
-            eos = []
-            r = random.random()
-
-            for i in range(len(input_ids)):
-                token, _, _ = ExLlamaV2Sampler.sample(logits[i:i + 1, :, :], settings[i], input_ids[i], r, tokenizer)
-                tempIDs = torch.cat([input_ids[i], token], dim=1)
-                input_ids[i] = tempIDs
-
-                token_count['gen_tokens'] += 1
-                token_count['total_tokens'] += 1
-                #stop_token = tokenizer.encode("</s>")
-                if token.item() == tokenizer.eos_token_id or caches[i].current_seq_len == caches[i].max_seq_len:
-                    if token.item() == settings[i].eos_token_id:
-                        print(f"Stopping for token: {token.item()}, settings eos: {settings[i].eos_token_id}, tokenizer eos: {tokenizer.eos_token_id}")
-                    eos.insert(0, i)  # Indices of completed prompts
-                    # Send the response immediately when a prompt is completed
-                    output = tokenizer.decode(input_ids[i])[0].strip()
-                    try:
-                        response_event = ids_lookup.pop(i, None)
-                        print(f"Response event is: {response_event}")
-                        #input_ids.pop(i)
-                        #caches.pop(i)
-                        #settings.pop(i)
-                        if response_event is not None:
-                            data = {"text": output}
-                            output = json.dumps(data, indent=2)
-                            if (args.verbose == True):
-                                print(output)
-                            response_event.set_result(output)
-                            #await asyncio.sleep(0.001)
-                            #break
-                        else:
-                            print(f"Response event is None")
-                    except Exception as e:
-                        print(f"Error processing completed prompt: {e}")
-                        continue
-
-
+                    #print(f"outputs.outputs {outputs[0].outputs[0]}")
                     #
+                    for output in outputs:
+                        data = {"text": output.outputs[0].text}
+                        jsond = json.dumps(data, indent=2)
+                        response_event.set_result(jsond)
                     continue
 
-                # Remove completed prompts from the lists
-            for i in eos:
-                #print(f"EOS#: {i}, {eos}")
-                try:
-                    #ids_lookup.pop(i, None)
-                    input_ids.pop(i)
-                    caches.pop(i)
-                    settings.pop(i)
-                except:
-                    print(f"Pop failed due to my crappy request lookup algorithm: {i}, {ids_lookup}")
+                prompt_count += ids.size(1)
+                batch_size = 1
+                if(args.cache_8bit):
+                    cache = ExLlamaV2Cache_8bit(model, max_seq_len=(ids.size(1) + max_tokens), batch_size = batch_size)
+                else:
+                    cache = ExLlamaV2Cache(model, max_seq_len=(ids.size(1) + max_tokens), batch_size = batch_size)
 
 
-        current_time = time.time()
-        time_elapsed_seconds = current_time - token_processing_start_time
-        total_processing_time += time_elapsed_seconds
-        read_speed = token_count['read_tokens'] / time_elapsed_seconds
-        generation_speed = token_count['gen_tokens'] / time_elapsed_seconds
-        average_gen_speed =  token_count['total_tokens'] / total_processing_time
+                if(args.lora):
+                    model.forward(ids[:, :-1], cache, preprocess_only=True, loras = loras)
+                else:
+                    model.forward(ids[:, :-1], cache, preprocess_only=True)
+                input_ids.append(ids)
+                caches.append(cache)
+                settings_clone = settings_proto.clone()
+                settings_clone.temperature = temperature
+                settings_clone.top_p = top_p
+                settings_clone.top_k = top_k
+                settings_clone.token_repetition_penalty = token_repetition_penalty
+                settings_clone.batch_size = 1
+                #settings_clone.disallow_tokens(tokenizer, [tokenizer.eos_token_id])
+                settings_clone.eos_token_id = 2
+                settings_clone.eos_token = "</s>"
 
-        # Log stats to the console
-        print(f"Batch process done. Read {token_count['read_tokens']} tokens at {read_speed:.2f} tokens/s. "
-              f"Generated {token_count['gen_tokens']} tokens at {generation_speed:.2f} tokens/s.\n"
-              f"This thread generated a total of {token_count['total_tokens']} tokens at {average_gen_speed:.2f} tokens/s.")
-        token_processing_start_time = None  # Reset the start time
+                #settings.append(settings_proto.clone())
+                settings.append(settings_clone)
+                ids_lookup[len(input_ids) - 1] = response_event #Should I change this to a hash? No because duplicates would still happen?
+            
+            # Just skip all this since it doesn't work if using the vllm engine
+
+            if(args.engine == "vLLM"):
+                continue
+
+            token_count['read_tokens'] += prompt_count
+            print(f"Doing input_ids at {time.time()}")
+            while input_ids:
+                inputs = torch.cat([x[:, -1:] for x in input_ids], dim=0)
+                if(args.lora):
+                    logits = model.forward(inputs, caches, input_mask=None, loras = loras).float().cpu()
+                else:
+                    logits = model.forward(inputs, caches, input_mask=None).float().cpu()
+                eos = []
+                r = random.random()
+
+                for i in range(len(input_ids)):
+                    token, _, _ = ExLlamaV2Sampler.sample(logits[i:i + 1, :, :], settings[i], input_ids[i], r, tokenizer)
+                    tempIDs = torch.cat([input_ids[i], token], dim=1)
+                    input_ids[i] = tempIDs
+
+                    token_count['gen_tokens'] += 1
+                    token_count['total_tokens'] += 1
+                    #stop_token = tokenizer.encode("</s>")
+                    if token.item() == tokenizer.eos_token_id or caches[i].current_seq_len == caches[i].max_seq_len:
+                        if token.item() == settings[i].eos_token_id:
+                            print(f"Stopping for token: {token.item()}, settings eos: {settings[i].eos_token_id}, tokenizer eos: {tokenizer.eos_token_id}")
+                        eos.insert(0, i)  # Indices of completed prompts
+                        # Send the response immediately when a prompt is completed
+                        output = tokenizer.decode(input_ids[i])[0].strip()
+                        try:
+                            if i in ids_lookup:
+                                response_event = ids_lookup.pop(i, None)
+                                if response_event is not None:
+                                    data = {"text": output}
+                                    output = json.dumps(data, indent=2)
+                                    if (args.verbose == True):
+                                        print(output)
+                                    response_event.set_result(output)
+                                else:
+                                    print(f"Response event is None")
+                            else:
+                                print(f"ID was not in ids_lookup")
+                        except Exception as e:
+                            print(f"Error processing completed prompt: {e}")
+                            continue
+
+
+                        #
+                        continue
+
+                    # Remove completed prompts from the lists
+
+                for i in eos:
+                    try:
+                        #ids_lookup.pop(i, None)
+                        input_ids.pop(i)
+                        caches.pop(i)
+                        settings.pop(i)
+                    except:
+                        print(f"Pop failed due to my crappy request lookup algorithm: {i}, {ids_lookup}")
+            #print(f"IDs lookup left over: {ids_lookup}")
+            try:
+                for i in ids_lookup:
+                    print(f"Jobs came back late, recovering: {ids_lookup}")
+                    response_event = ids_lookup.pop(i, None)
+                    if response_event is not None:
+                        data = {"text": output}
+                        output = json.dumps(data, indent=2)
+                        if (args.verbose == True):
+                            print(output)
+                        response_event.set_result(output)
+                    else:
+                        print(f"Response event is None")
+            except Exception as e:
+                print(f"Error processing completed prompt: {e}")
+                continue
+
+
+            current_time = time.time()
+            time_elapsed_seconds = current_time - token_processing_start_time
+            total_processing_time += time_elapsed_seconds
+            read_speed = token_count['read_tokens'] / time_elapsed_seconds
+            generation_speed = token_count['gen_tokens'] / time_elapsed_seconds
+            average_gen_speed =  token_count['total_tokens'] / total_processing_time
+
+            # Log stats to the console
+            print(f"Batch process done. Read {token_count['read_tokens']} tokens at {read_speed:.2f} tokens/s. "
+                  f"Generated {token_count['gen_tokens']} tokens at {generation_speed:.2f} tokens/s.\n"
+                  f"This thread generated a total of {token_count['total_tokens']} tokens at {average_gen_speed:.2f} tokens/s.")
+            token_processing_start_time = None  # Reset the start time
+except:
+    print("Model processing died. Attempting reload.")
+    setup_model()
+    asyncio.create_task(inference_loop())
 
 
 @app.get('/')
@@ -245,21 +297,37 @@ def read_root():
 @app.post('/generate', response_class=PlainTextResponse)
 async def generate(prompt: PromptRequest):
     global prompts_queue, results, token_count
+    if(args.engine == "vLLM"):
+        encoded_prompt = prompt.prompt
+        token_count['prompt_tokens'] += len(encoded_prompt) - 1
+    else:
+        encoded_prompt = tokenizer.encode(prompt.prompt)
+        token_count['prompt_tokens'] += len(encoded_prompt) - 1
 
-    encoded_prompt = tokenizer.encode(prompt.prompt)
-    token_count['prompt_tokens'] += len(encoded_prompt) - 1
     completion_event = asyncio.Future()
-    await prompts_queue.put((encoded_prompt, completion_event, prompt.max_tokens, prompt.temperature, prompt.top_k, prompt.top_p, prompt.token_repetition_penalty, prompt.stop))
+    await prompts_queue.put((encoded_prompt, completion_event, prompt.max_tokens, prompt.temperature, prompt.top_k, prompt.top_p, prompt.token_repetition_penalty, prompt.stop, prompt.prompt))
 
     try:
         # Wait until the prompt is processed or timeout occurs
-        return await asyncio.wait_for(completion_event, timeout=args.timeout)
+        if(args.timeout):
+            return await asyncio.wait_for(completion_event, timeout=args.timeout)
+        else:
+            return await asyncio.wait_for(completion_event)
     except asyncio.TimeoutError:
         raise HTTPException(status_code=504, detail="Processing the prompt timed out.")
 
 
 def setup_model():
     global model, tokenizer, loras
+    
+    if(args.engine == "vLLM"):
+        import ray, torch
+        ray.shutdown()
+        ray.init(num_gpus=torch.cuda.device_count())
+        model = LLM(model=args.model,max_context_len_to_capture=args.max_model_len,tensor_parallel_size=2)
+        #model = LLM(model=args.model,max_context_len_to_capture=args.max_model_len)
+        return
+
     model_directory = args.model
     config = ExLlamaV2Config()
     config.model_dir = model_directory
